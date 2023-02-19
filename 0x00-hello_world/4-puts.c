2426d0c3012f6708c70d7e746a1e93c870dd9e18
Mhuyiwa <mhuyi@yahoo.com>
2023-02-18 23:39:19 -0800

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
 /**
- * main - prints "Programming is like building a multilingual 
- * puzzle, followed by a newline."
- * return 0.
- */
-
+*main - contain the major code
+*return : will return 0.
+*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");	
-		
-	return(0);
-
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
-
+/**
+*main - contain the major code
+*return : will always return 0
+*/
 int main(void)
-
 {
 	printf("with proper grammar, but the outcome is a pieces of art, \n");
-
 	return (0);
-
 }
