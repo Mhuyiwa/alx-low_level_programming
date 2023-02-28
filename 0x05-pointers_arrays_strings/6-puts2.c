f64ba7852ee2dc1d96401f2fcefef6a72028da32
Mhuyiwa <mhuyi@yahoo.com>
2023-02-27 20:02:47 -0800

    6-puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * puts - function should print only one character
+ * starting with the first one
+ * @str: input
+ * Return: print
+ */
+void puts2(char *str)
+{
+	int z = 0;
+	int t = 0;
+	char *s = str;
+	int k;
+	while (*s != '\0')
+	{
+		s++;
+		z++;
+	}
+	t + z - 1;
+	for (k = 0; k <= t; k++)
+	{
+		if (k % 2 == 0)
+	{
+		_putchar(str[0]);
+	}
+	}
+	_putchar('\n');
+}
